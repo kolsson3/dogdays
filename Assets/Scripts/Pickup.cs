beb65a1d3aec8f9a28b62d0a437959fdec35f45c
Kris <kolsson333@gmail.com>
2020-10-14 21:57:33 -0700

    Kris room first commit

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class Pickup : MonoBehaviour
 {
     public Transform destination;
-    public float grabThreshold = 5f;
+    public float grabThreshold = 1f;
+    bool grabbed = false;
 
     void OnMouseDown()
     {
-        Vector3 Dest = GameObject.Find("Dest").transform.position;
+        Vector3 Dest = GameObject.Find("Target").transform.position;
         float distance = Vector3.Distance(Dest, this.transform.position);
         Debug.Log(distance); 
         if(distance <= grabThreshold)
         {
+            grabbed = true;
             GetComponent<Rigidbody>().useGravity = false;
             GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionZ
                                         | RigidbodyConstraints.FreezePositionX
@@ -22,15 +24,20 @@ public class Pickup : MonoBehaviour
                                         | RigidbodyConstraints.FreezeRotationY
                                         | RigidbodyConstraints.FreezeRotationZ;
             this.transform.position = destination.position;
-            this.transform.parent = GameObject.Find("Dest").transform;
+            this.transform.parent = GameObject.Find("Target").transform;
         }
     }
 
     void OnMouseUp()
     {
-        this.transform.parent = null;
-        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-        GetComponent<Rigidbody>().useGravity = true;
+        if (grabbed)
+        {
+            this.transform.parent = null;
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            GetComponent<Rigidbody>().useGravity = true;
+            grabbed = false;
+        }
+        
     }
 
 }
