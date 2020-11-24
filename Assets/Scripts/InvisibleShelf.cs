e2dcdb866e3d33501dd15758a2df89d3c4df46cd
pavi102 <pavi1022@gmail.com>
2020-11-24 10:21:18 -0800

    added garage-room

---
+++ Assets/Scripts/InvisibleShelf.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisibleShelf : MonoBehaviour
+{
+    public float FloatStrenght;
+     public float RandomRotationStrenght;
+     
+     
+     void Update () {
+         transform.GetComponent<Rigidbody>().AddForce(Vector3.up *FloatStrenght);
+          transform.Rotate(RandomRotationStrenght,RandomRotationStrenght,RandomRotationStrenght);
+     }
+}
