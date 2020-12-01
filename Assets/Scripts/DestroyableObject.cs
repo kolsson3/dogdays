335d048095d6ef2f67c14a1f74a05efb2d58707d
pavi102 <pavi1022@gmail.com>
2020-12-01 15:57:43 -0800

    Added some breakable objects

---
+++ Assets/Scripts/DestroyableObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyableObject : MonoBehaviour
+{
+    public GameObject destroyedObject;
+
+    private void OnCollisionEnter(Collision collision) {
+        
+        if (collision.collider.CompareTag("Floor"))
+        {
+            Instantiate (destroyedObject, transform.position, transform.rotation);
+            Destroy(this.gameObject);
+        }
+    }
+}
